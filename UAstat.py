830c0569b3a44cf7eedce8ce732270290b2c4962
cyz14 <icyz14@163.com>
2016-03-27 22:41:34 +0800

    simple add some lines

--- UAstat.py
+++ UAstat.py
@@ -6,13 +6,25 @@ import json
 import httpagentparser
 import ipdata
 
+def error(error_info):
+	print 'Error:', error_info
+	return
+	
 
 def main():
 	# print 'httpagentparser version', httpagentparser.__version__
 	# print len(sys.argv)
 	# raw_input()
+	if len(sys.argv) != 2:
+		error('No input file.')
+		return -1
+	
 	file_name = sys.argv[1]
 	data_file = open(file_name, 'r')
+	if not data_file:
+		error('File not opened.')
+		return -1
+		
 	empty = 0
 	hit   = 0
 	for line in data_file:
@@ -30,17 +42,16 @@ def main():
 	print empty, hit
 
 
+def test(file_name):
+	file_name
+
 def ip_to_int(ip_addr):
     array = ip_addr.split('.')
     if len(array) != 4:
-        print 'Error: ip_addr not valid'
+        error('ip_addr not valid.')
         return 0
     
-    ans = 0
-    for i in array:
-        ans *= 256
-        ans += int(i)
-        
+    ans = reduce(lambda x, y: int(x)*256 + int(y), array)
     return ans
 
 def int_to_ip(ip_int):
--- ipdata.py
+++ ipdata.py
@@ -1,22 +1,25 @@
 class ipdata():
-	"""store the info of os, browser, service type and data size distribution belonging to a specific ip address"""
-	def __init__(self, ip_addr):
-		super(ipdata, self).__init__()
-		self.ip_addr = ip_addr
-
-	self.os = {}
-	self.browser = {}
-	self.data_size = {}
-	self.service = {}
+	"""store the info of os, browser, service 
+	type and data size distribution belonging 
+	to a specific ip address"""
 	
-	def update_os():
-	pass
-
-	def update_browser():
+	def __init__(self, ip_addr):
+	#	super(ipdata, self).__init__()
+		self.ip_addr = ip_addr
+		self.os = {}
+		self.browser = {}
+		self.data_size = {}
+		self.service = {}
+		
+	
+	def update_os(os):
 		pass
 
-	def update_data_size():
+	def update_browser(browser):
 		pass
 
-	def update_service():
+	def update_data_size(size):
+		pass
+
+	def update_service(service):
 		pass
