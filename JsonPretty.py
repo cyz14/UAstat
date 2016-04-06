64d66b3c77f5b1a3d383ab293f06613c8d5ae587
cyz14 <icyz14@163.com>
2016-04-06 13:18:35 +0800

    Complete JsonPretty.py

--- JsonPretty.py
+++ JsonPretty.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import json, os
+import json, sys, os
 
-class JsonPretty(object):
+class JsonPretty():
 	"""docstring for JsonPretty"""
 	def __init__(self, file_name):
 		self.file_name = file_name
@@ -12,15 +12,20 @@ class JsonPretty(object):
 		self.file_name = file_name
 
 	def pretty_file(self):
-		tmp_file = open('.tmp.json', 'w')
+		tmp_file = open('__tmp.json', 'w')
 
 		for line in open(self.file_name, 'r'):
 			req = json.loads(line)
-			tmp_file.write( json.dumps(req, separators=(',', ':')) + '\n')
+			tmp_file.write( json.dumps( req, indent=2, separators=( ',', ':' ) ) + '\n')
 
-		# os.rename(self.file_name, '_' + self.file_name)
-		# os.rename('.tmp.json', self.file_name)
+		os.rename( self.file_name, ''.join( [ self.file_name , '.bak' ]))
+		os.rename( '__tmp.json' , self.file_name )
 
+
+def main():
+	file_name = sys.argv[1]
+	jsonpretty = JsonPretty(file_name)
+	jsonpretty.pretty_file()
 
 if __name__ == '__main__':
 	main()
