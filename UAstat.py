a0cfc026b9176f3c433beeb73031f1b6991f5ef3
cyz14 <icyz14@163.com>
2016-04-08 02:24:52 +0800

    UAstat.py and ipdata.py Updated and almost completed

--- UAstat.py
+++ UAstat.py
@@ -6,73 +6,53 @@ import json
 from httpagentparser import detect
 from MergeSesReq import *
 from ipdata import *
+import numpy as np
+
+
+ip_db = {}
+log = open('req.log', 'w')
 
 def error(error_info):
 	print 'Error:', error_info
 	return
 
-ip_db = {}
-
-
 def main():
-
 	if len(sys.argv) < 2:
 		error('No input file.')
 		return -1
 	
 	file_name = sys.argv[1]
 	work(file_name)
-	
-	
-def deal_session_file(file_name='session.json'):
-	data_file = open(file_name, 'r')
-	if not data_file:
-		error('File not opened.')
-		return -1
-
-	for line in data_file:
-		req = json.loads(line)
-		req_id = ip_to_int(req['ClientIP'])
-		rtt = req['Rtt']
-		if req_id not in ip_db:
-			ip_db[req_id] = new ipdata()
-
-def deal_request_file(file_name='request.json'):
-	data_file = open(file_name, 'r')
-	if not data_file:
-		error('File not opened.')
-		return -1
-	
-	log = open('req.log', 'w')
-
-	empty = 0
-	hit   = 0
-	for line in data_file:
-		req = json.loads(line)
-		if req["UserAgent"]:
-			hit += 1
-			process_request(req)
-		else:
-			empty += 1
-	print empty, hit
 	write_stat(ip_db)
-	log.close()
-	data_file.close()
-	return
+
 
 def process_request(req):
+	"""
+	update os, browser and service under ip
+	"""
 	client_ip = ip_to_int(req['ClientIP'])
 	result = detect(req['UserAgent'])
-	ip_db[client_ip] = ipdata(client_ip)
-	if not ip_db[client_ip].update(result):
+	if client_ip not in ip_db:
+		ip_db[client_ip] = ipdata(client_ip)
+	# else ipdata(client_ip) already in ip_db
+
+	if not ip_db[client_ip].update_os_browser(result):
 		log.write(req['UserAgent'] + '\n' + str(result) + '\n\n')
 
-	ip_db[client_ip].update_data_size(req["BodyLen"])
+	# ip_db[client_ip].update_data_size(req["BodyLen"])
 	ip_db[client_ip].update_service(req["ServiceName"])
 
 
 def process_session(ses):
+	"""
+	update rtt, retransrate, latencyinfo{ latency, data_size, timeout}
+	"""
+	client_ip = ip_to_int(ses['ClientIP'])
 	
+	if client_ip not in ip_db:
+		ip_db[client_ip] = ipdata(client_ip)
+
+	ip_db[client_ip].update_from_session(ses)
 
 
 def deal_merged_file(file_name='session.request.json'):
@@ -83,19 +63,24 @@ def deal_merged_file(file_name='session.request.json'):
 
 	for index, line in enumerate(fin):
 		Log = json.loads(line)
-		Req = Log['ReqLog']
-		process_request(Req)
+		Reqs = Log['ReqLog']
+		for req in Reqs:
+			process_request(req)
 
 		Ses = Log['SesLog']
+		# print Ses['LatencyInfo'], type(Ses['LatencyInfo'])
+		# if raw_input().lower().startswith('q'):
+		# 	return
 		process_session(Ses)
+
+	fin.close()
 
 
 def work(file_name):
-	#MergeSesReq()
 	# deal_session_file()
 	# deal_request_file()
+	deal_merged_file(file_name)
 	
-
 
 def write_stat(db, file_name='stat.json'):
 	stat_file = open(file_name, 'w')
@@ -109,4 +94,36 @@ if __name__ == '__main__':
 	main()
 
 
+def deal_request_file(file_name='request.json'):
+	data_file = open(file_name, 'r')
+	if not data_file:
+		error('File not opened.')
+		return -1
 
+	empty = 0
+	hit   = 0
+	for line in data_file:
+		req = json.loads(line)
+		if req["UserAgent"]:
+			hit += 1
+			process_request(req)
+		else:
+			empty += 1
+	print empty, hit
+	
+	data_file.close()
+	return
+
+
+def deal_session_file(file_name='session.json'):
+	data_file = open(file_name, 'r')
+	if not data_file:
+		error('File not opened.')
+		return -1
+
+	for line in data_file:
+		ses = json.loads(line)
+		ses_id = ip_to_int(ses['ClientIP'])
+		rtt = ses['Rtt']
+		if ses_id not in ip_db:
+			ip_db[ses_id] = ipdata(client_ip)
--- ipdata.py
+++ ipdata.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import json
+# from pylab import *
+from scipy import stats
+import matplotlib.pyplot as plt
+from CdfQuantiles import cdf_quantiles
 
 def ip_to_int(ip_addr):
     array = ip_addr.split('.')
@@ -33,28 +37,58 @@ class ipdata():
 		Store the info of os, browser, service type and data size 
 		distribution belonging to a specific ip address
 		"""
-	# super(ipdata, self).__init__()
-		self.ip_addr = ip_addr
-		self.os = {}
+		self.MIN_SAMPLE_RATE = 10
+
+		self.ip_addr = ip_addr	# here ip_addr is integer
+
+		# in request.UserAgent, from httpagentparser.detect()
+		self.os = {}				
 		self.browser = {}
-		self.data_size = {}
 		self.service = {}
+
+		# in session
+		self.rtt_list = []			# Rtt
+		self.retransrate = 0		# retransmit rate
+
+		# in session.LatencyInfo
+		self.latency_list = []		# .TcpLatency
+		self.data_size_list = []	# .TransDataSize
+		self.timeout = 0			# .TimeOut
+
 
 	def __reqr__(self):
 		return "ipdata()"
 
 	def __str__(self):
 		'''
-		return the content of the ip object
+		return the content of the ip
 		'''
-		return json.dumps(
-			{'ip_addr': int_to_ip(self.ip_addr),
-			'os':self.os, 
-			'browser':self.browser, 
-			'data_size':self.data_size, 
-			'service':self.service});
-	
-	def update(self, result):
+		ans = {
+			'ip_addr':int_to_ip(self.ip_addr),
+			'os':self.os,
+			'browser':self.browser,
+			'service':self.service,
+			'retransrate':1.0*self.retransrate/len(self.rtt_list),	# average
+			'timeout':1.0*self.timeout/len(self.rtt_list)			# average
+			}
+		if self.session_number() >= self.MIN_SAMPLE_RATE:
+			ans['rtt'] 		= cdf_quantiles(self.rtt_list)
+			ans['latency'] 	= cdf_quantiles(self.latency_list)
+			ans['data_size']= cdf_quantiles(self.data_size_list)
+		else:
+			ans['rtt']		= self.rtt_list
+			ans['latency']  = self.latency_list
+			ans['data_size']= self.data_size_list
+
+		return json.dumps(ans)
+
+	def session_number(self):
+		return len(self.rtt_list)
+
+	def set_min_sample_rate(size):
+		self.MIN_SAMPLE_RATE = size
+
+	def update_os_browser(self, result):
 		ans = True
 		if 'os' not in result and 'browser' not in result:
 			self.update_browser({'name':None})
@@ -93,10 +127,7 @@ class ipdata():
 			return True
 		else:
 			return False
-		
-
-			
-
+	
 
 	def update_os(self, os):
 		if os['name'] not in self.os:
@@ -108,13 +139,22 @@ class ipdata():
 			self.browser[browser['name']] = 0
 		self.browser[browser['name']] += 1
 
-	def update_data_size(self, data_size):
-		if data_size not in self.data_size:
-			self.data_size[data_size] = 0
-		self.data_size[data_size] += 1
-
 	def update_service(self, service_name):
 		if service_name not in self.service:
 			self.service[service_name] = 0
 		self.service[service_name] += 1
 
+	def update_data_size(self, data_size):
+		self.data_size_list.append(data_size)
+
+	def update_rtt(self, rtt):
+		self.rtt_list.append(rtt)
+
+	def update_latency(self, latency):
+		self.latency_list.append(latency)
+
+	def update_from_session(self, ses):
+		self.update_rtt(ses['Rtt'])
+		for info in ses['LatencyInfo']:			
+			self.update_latency(info['TcpLatency'])
+			self.update_data_size(ses['TransmitSize'])
